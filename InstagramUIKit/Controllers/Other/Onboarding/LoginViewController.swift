5594c4397f981ae398162e33951b981680cf20a1
Carlos <morales22@outlook.com>
2021-11-16 09:46:02 -0600

    added checks for username or email input into username field, added alerts for failed logins, added functionality to auth service class

--- InstagramUIKit/Controllers/Other/Onboarding/LoginViewController.swift
+++ InstagramUIKit/Controllers/Other/Onboarding/LoginViewController.swift
@@ -200,6 +200,33 @@ class LoginViewController: UIViewController {
 		guard let usernameEmail = usernameEmailField.text, !usernameEmail.isEmpty, let password = passwordField.text, !password.isEmpty, password.count >= 8 else {
 			return
 		}
+		
+		var username: String?
+		var email: String?
+		
+		if usernameEmail.contains("@"), usernameEmail.contains(".") {
+			// email
+			email = usernameEmail
+		} else {
+			// username
+			username = usernameEmail
+		}
+		
+		AuthManager.shared.loginUser(username: username, email: email, password: password) { success in
+			DispatchQueue.main.async {
+				if success {
+					// user logged in
+					self.dismiss(animated: true, completion: nil)
+				} else {
+					// error logging in
+					let alert = UIAlertController(title: "Log In Error", message: "We were unable to log you in", preferredStyle: .alert)
+					alert.addAction(UIAlertAction(title: "Dismiss",
+												  style: .cancel,
+												  handler: nil))
+					self.present(alert, animated: true)
+				}
+			}
+		}
 	}
 	@objc private func didTapTermsButton() {
 		guard let url = URL(string: "https://help.instagram.com/581066165581870") else {
